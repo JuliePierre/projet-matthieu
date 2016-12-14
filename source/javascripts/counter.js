e4ef5150442a8250f566796a5832467eeffd96b1
Julie Pierre <julie.pierre3@gmail.com>
2016-12-14 10:37:46 +0100

    addind cloudinary

--- source/javascripts/counter.js
+++ source/javascripts/counter.js
@@ -1,10 +1,22 @@
-$(function(){
-  var interval = setInterval(increment, 8000); // 1000ms = 1 second
-
-  function increment() {
-    console.log('titi')
-    for (var i = 0; i < 200; i += 1) {
-      $('#counter').val(i);
-    }
-  }
+$( document ).ready(function() {
+  console.log('titi');
+  $('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 3000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+    });
+  });
 });
+
