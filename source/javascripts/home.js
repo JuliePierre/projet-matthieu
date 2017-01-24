944837afa9bbf7c3ea13d718ed834b12deba7e5f
Julie Pierre <julie.pierre3@gmail.com>
2017-01-24 18:49:33 +0100

    WIP

--- source/javascripts/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#point-blanc').click(function(){
-    $('html,body').animate({
-        scrollTop: $("#first-content").position().top - 60 },
-        1000);
-  });
-});
--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -8,3 +8,20 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  var page = $('nav')[0].id.split("-")[0];
+  console.log('page')
+  var links = $('nav ul a')
+  links.each(function(i, item) {
+    console.log(item.getAttribute("data-info"));
+    if (item.getAttribute("data-info") == page) {
+      $(item).addClass('brand-red-text');
+      $(item).addClass('semi-gras');
+    };
+    if (item.getAttribute("data-info") != page) {
+      $(item).removeClass('brand-red-text');
+      $(item).removeClass('semi-gras');
+    };
+  });
+});
