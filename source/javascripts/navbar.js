9311568fd0125589fa8e13f71bb9503539b2f104
Julie Pierre <julie.pierre3@gmail.com>
2017-01-25 11:51:27 +0100

    WIP

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 65) {
-      $('#index-navbar').removeClass('index');
-    }
-    if ($(window).scrollTop() < 65) {
-      $('#index-navbar').addClass('index');
-    }
-  });
-});
-
-$(document).ready(function() {
   var page = $('nav')[0].id.split("-")[0];
   console.log('page')
   var links = $('nav ul a')
@@ -17,11 +6,20 @@ $(document).ready(function() {
     console.log(item.getAttribute("data-info"));
     if (item.getAttribute("data-info") == page) {
       $(item).addClass('brand-red-text');
-      $(item).addClass('semi-gras');
+      $(item).addClass('gras');
     };
     if (item.getAttribute("data-info") != page) {
       $(item).removeClass('brand-red-text');
-      $(item).removeClass('semi-gras');
+      $(item).removeClass('gras');
     };
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 65) {
+      $('#index-navbar').removeClass('index');
+    }
+    if ($(window).scrollTop() < 65) {
+      $('#index-navbar').addClass('index');
+    }
   });
 });
