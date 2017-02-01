c43259dde973dc75a5e9ec3041d0d09751f6670b
Julie Pierre <julie.pierre3@gmail.com>
2017-02-01 16:15:09 +0100

    changed home page

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   var page = $('nav')[0].id.split("-")[0];
-  console.log('page')
   var links = $('nav ul a')
   links.each(function(i, item) {
-    console.log(item.getAttribute("data-info"));
     if (item.getAttribute("data-info") == page) {
       $(item).addClass('brand-red-text');
       $(item).addClass('gras');
