29471d51d02abb8a7ac709123ead351076d6a94f
Julie Pierre <julie.pierre3@gmail.com>
2018-11-01 16:31:44 +0100

    carousel d'images mediatheque ok

--- source/javascripts/slick.js
+++ source/javascripts/slick.js
@@ -3015,10 +3015,33 @@
 $(document).ready(function(){
   $('.carousel-slick').slick({
     centerMode: true,
-    slidesToShow: 5,
-    focusOnSelect: true,
-    swipe: true,
-    respondTo: "min",
-    centerPadding: '0px'
+    centerPadding: '220px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      }
+    ]
+    // centerMode: true,
+    // slidesToShow: 5,
+    // focusOnSelect: true,
+    // swipe: true,
+    // respondTo: "min",
+    // centerPadding: '0px'
   });
 });
